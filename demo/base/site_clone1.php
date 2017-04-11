1d88aed6c0bf4874f58cfde41b928c06b969f182
luobo157 <luobo157@126.com>
2017-04-11 16:06:59 +0800

    Update site_clone1.php

--- demo/base/site_clone1.php
+++ demo/base/site_clone1.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('memory_limit', '2048M');  //设置运行时候的内存限制，克隆网站较大的时候可以使用，较小则随意
 require_once '../vendor/autoload.php';
 use Ares333\CurlMulti\AutoClone;
 $url = array(
@@ -16,7 +17,7 @@ if (! file_exists($cacheDir)) {
 }
 $clone = new AutoClone($url, $dir);
 $clone->overwrite = true;
-$clone->getCurl()->maxThread = 3;
+$clone->getCurl()->maxThread = 3;  //并发数量，默认为3
 $clone->getCurl()->cache['enable'] = true;
 $clone->getCurl()->cache['enableDownload'] = true;
 $clone->getCurl()->cache['dir'] = $cacheDir;
